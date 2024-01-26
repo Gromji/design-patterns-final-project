baeea735267e58e1fd864cf5c4c5254a13647c7d
Gromji <garse20@freeuni.edu.ge>
2024-01-26 22:09:13 +0400

    Minor code update.

--- wallet/infra/repository/sqlite/user_repository.py
+++ wallet/infra/repository/sqlite/user_repository.py
@@ -1,3 +1,4 @@
+from contextlib import closing
 from uuid import UUID
 
 from wallet.core.entity.user import User
@@ -11,39 +12,43 @@ USER_TABLE_NAME = "users"
 class UserRepository(IUserRepository):
     def __init__(self) -> None:
         with ConnectionManager.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                f"""CREATE TABLE IF NOT EXISTS {USER_TABLE_NAME} (
-                    id TEXT PRIMARY KEY, email TEXT UNIQUE, api_key TEXT UNIQUE
-                    )"""
-            )
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"""CREATE TABLE IF NOT EXISTS {USER_TABLE_NAME} (
+                        id TEXT PRIMARY KEY, email TEXT UNIQUE, api_key TEXT UNIQUE
+                        )"""
+                )
 
     def get_user_by_id(self, user_id: UUID) -> User:
         with ConnectionManager.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM {USER_TABLE_NAME} WHERE id = ?", (user_id,))
-            user = cursor.fetchone()
-            if user is None:
-                raise DoesNotExistError(f"User with id {user_id} not found")
-            return User(user_id=UUID(user[0]), email=user[1], api_key=user[2])
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"SELECT * FROM {USER_TABLE_NAME} WHERE id = ?", (user_id,)
+                )
+                user = cursor.fetchone()
+                if user is None:
+                    raise DoesNotExistError(f"User with id {user_id} not found")
+                return User(user_id=UUID(user[0]), email=user[1], api_key=user[2])
 
     def get_user_by_email(self, email: str) -> User:
         with ConnectionManager.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM {USER_TABLE_NAME} WHERE email = ?", (email,))
-            user = cursor.fetchone()
-            if user is None:
-                raise DoesNotExistError(f"User with email {email} not found")
-            return User(user_id=UUID(user[0]), email=user[1], api_key=user[2])
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"SELECT * FROM {USER_TABLE_NAME} WHERE email = ?", (email,)
+                )
+                user = cursor.fetchone()
+                if user is None:
+                    raise DoesNotExistError(f"User with email {email} not found")
+                return User(user_id=UUID(user[0]), email=user[1], api_key=user[2])
 
     def create_user(self, user: User) -> User:
         with ConnectionManager.get_connection() as conn:
-            cursor = conn.cursor()
             try:
-                cursor.execute(
-                    f"INSERT INTO {USER_TABLE_NAME} VALUES (?, ?, ?)",
-                    (str(user.user_id), user.email, user.api_key),
-                )
+                with closing(conn.cursor()) as cursor:
+                    cursor.execute(
+                        f"INSERT INTO {USER_TABLE_NAME} VALUES (?, ?, ?)",
+                        (str(user.user_id), user.email, user.api_key),
+                    )
             except Exception as e:
                 raise AlreadyExistsError(
                     f"User with id {user.user_id} already exists"
@@ -58,6 +63,5 @@ class UserRepository(IUserRepository):
 
     def tear_down(self) -> None:
         with ConnectionManager.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(f"DROP TABLE {USER_TABLE_NAME}")
-            cursor.close()
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(f"DROP TABLE {USER_TABLE_NAME}")
