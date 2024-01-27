d1da8d52dc2e2347deeef9ad4c9b4146b7ec2c83
Saba-SH <sshos20@freeuni.edu.ge>
2024-01-27 17:31:20 +0400

    implement transaction repository

--- wallet/infra/repository/memory/transaction_repository.py
+++ wallet/infra/repository/memory/transaction_repository.py
@@ -1,25 +1,33 @@
-from typing import List
+from typing import List, Dict
 from uuid import UUID
 
 from wallet.core.entity.transaction import Transaction
+from wallet.core.error.errors import DoesNotExistError, AlreadyExistsError
 from wallet.infra.repository.repository_interface import ITransactionRepository
 
 
 class TransactionRepository(ITransactionRepository):
     def __init__(self) -> None:
-        pass
+        self.transactions: Dict[UUID, Transaction] = {}
 
     def get_transaction_by_id(self, transaction_id: UUID) -> Transaction:
-        pass
+        try:
+            return self.transactions[transaction_id]
+        except KeyError:
+            raise DoesNotExistError(f"Transaction with id {str(transaction_id)} not found")
 
     def get_all_transactions(self) -> List[Transaction]:
-        pass
+        return [v for v in self.transactions.values()]
 
     def create_transaction(self, transaction: Transaction) -> Transaction:
-        pass
+        if transaction.id in self.transactions:
+            raise AlreadyExistsError(f"Transaction with id {str(transaction.id)} already exists")
+
+        self.transactions[transaction.id] = transaction
+        return transaction
 
     def filter_transactions(self, wallet: "Wallet") -> List[Transaction]:
         pass
 
     def tear_down(self) -> None:
-        pass
+        self.transactions = {}
--- wallet/infra/repository/sqlite/transaction_repository.py
+++ wallet/infra/repository/sqlite/transaction_repository.py
@@ -1,25 +1,86 @@
+from contextlib import closing
 from typing import List
 from uuid import UUID
 
 from wallet.core.entity.transaction import Transaction
+from wallet.core.error.errors import DoesNotExistError, AlreadyExistsError
 from wallet.infra.repository.repository_interface import ITransactionRepository
+from wallet.infra.repository.sqlite.connection_manager import ConnectionManager
+
+TRANSACTION_TABLE_NAME = "transactions"
 
 
 class TransactionRepository(ITransactionRepository):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, isolated: bool = False) -> None:
+        with ConnectionManager.get_connection() as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"""CREATE TABLE IF NOT EXISTS Transactions (
+                        id TEXT PRIMARY KEY,
+                        From_Address TEXT,
+                        To_Address TEXT,
+                        Amount INTEGER,
+                        Fee INTEGER,
+                        {'''FOREIGN KEY (From_Address) REFERENCES Wallets(Address),
+                        FOREIGN KEY (To_Address) REFERENCES Wallets(Address)'''
+                    if not isolated else ''}
+                    );"""
+                )
 
     def get_transaction_by_id(self, transaction_id: UUID) -> Transaction:
-        pass
+        with ConnectionManager.get_connection() as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"SELECT * FROM {TRANSACTION_TABLE_NAME} WHERE id = ?", (transaction_id,)
+                )
+                transaction = cursor.fetchone()
+                if transaction is None:
+                    raise DoesNotExistError(f"Transaction with id {str(transaction_id)} not found")
+                return Transaction(id=transaction[0],
+                                   from_address=transaction[1], to_address=transaction[2],
+                                   amount=transaction[3], fee=transaction[4])
 
     def get_all_transactions(self) -> List[Transaction]:
-        pass
+        with ConnectionManager.get_connection() as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"SELECT * FROM {TRANSACTION_TABLE_NAME}"
+                )
+                transactions = cursor.fetchall()
+                return [Transaction(id=t[0],
+                                    from_address=t[1], to_address=t[2],
+                                    amount=t[3], fee=t[4])
+                        for t in transactions]
 
     def create_transaction(self, transaction: Transaction) -> Transaction:
-        pass
+        with ConnectionManager.get_connection() as conn:
+            try:
+                with closing(conn.cursor()) as cursor:
+                    cursor.execute(
+                        f"INSERT INTO {TRANSACTION_TABLE_NAME} VALUES (?, ?, ?, ?, ?)",
+                        (str(transaction.id), transaction.from_address,
+                         transaction.to_address, transaction.amount, transaction.fee),
+                    )
+            except Exception as e:
+                raise AlreadyExistsError(
+                    f"Transaction with id {transaction.id} already exists"
+                ) from e
+            return transaction
 
     def filter_transactions(self, wallet: "Wallet") -> List[Transaction]:
-        pass
+        with ConnectionManager.get_connection() as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(
+                    f"SELECT * FROM {TRANSACTION_TABLE_NAME}"
+                )
+                transactions = cursor.fetchall()
+                return [Transaction(id=t[0],
+                                    from_address=t[1], to_address=t[2],
+                                    amount=t[3], fee=t[4])
+                        for t in transactions if wallet.address in (t[1], t[2])]
 
     def tear_down(self) -> None:
-        pass
+        with ConnectionManager.get_connection() as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute(f"DROP TABLE {TRANSACTION_TABLE_NAME}")
+
