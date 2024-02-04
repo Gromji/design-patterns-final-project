e9647645268312bc4e6f736aee3a3ad75ecc34a7
Giorgi Megrelidze <gmegr20@freeuni.edu.ge>
2024-02-04 16:04:22 +0400

    Fixed transaction list response

--- wallet/infra/fastapi/transactions_api.py
+++ wallet/infra/fastapi/transactions_api.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
-from wallet.core.entity.transaction import Transaction, TransactionBuilder
+from wallet.core.entity.transaction import TransactionBuilder
 from wallet.infra.fastapi.dependables import (
     UserServiceDependable,
     WalletServiceDependable,
@@ -12,16 +12,19 @@ from wallet.infra.fastapi.dependables import (
 transactions_api = APIRouter()
 
 
+class TransactionResponse(BaseModel):
+    id: str
+    from_address: str
+    to_address: str
+    amount: int
+    fee: int
+
+
 class TransactionListResponse(BaseModel):
-    transactions_list: list
-
-
-class ApiRequest(BaseModel):
-    api_key: str
+    transaction_list: list[TransactionResponse]
 
 
 class MakeTransactionRequest(BaseModel):
-    api_key: str
     sender_wallet_address: str
     receiver_wallet_address: str
     amount: int
@@ -29,19 +32,28 @@ class MakeTransactionRequest(BaseModel):
 
 @transactions_api.get("/", status_code=201, response_model=TransactionListResponse)
 def list_transactions(
-    list_request: ApiRequest,
     user_service: UserServiceDependable,
     wallet_service: WalletServiceDependable,
     transaction_service: TransactionServiceDependable,
-) -> list[Transaction] | JSONResponse:
+    api_key: str = Header(..., convert_underscores=False),
+) -> dict[str, list[dict[str, str | int]]] | JSONResponse:
     try:
-        user = user_service.get_user_by_api_key(list_request.api_key)
+        user = user_service.get_user_by_api_key(api_key)
         wallets = wallet_service.get_user_wallets(user)
-        transactions: list[Transaction] = []
+        transactions = []
         for wallet in wallets:
             transactions += transaction_service.filter_transactions(wallet)
 
-        return transactions
+        response = [ 
+        {
+            "id": str(t.transaction_id),
+            "from_address": t.from_address,
+            "to_address": t.to_address,
+            "amount": t.amount,
+            "fee": t.fee
+        } for t in transactions]
+
+        return {"transaction_list": response}
     except Exception as err:
         return JSONResponse(
             status_code=409,
@@ -54,9 +66,10 @@ def make_transaction(
     make_transaction_request: MakeTransactionRequest,
     user_service: UserServiceDependable,
     transaction_service: TransactionServiceDependable,
+    api_key: str = Header(..., convert_underscores=False),
 ) -> None | JSONResponse:
     try:
-        issuer = user_service.get_user_by_api_key(make_transaction_request.api_key)
+        issuer = user_service.get_user_by_api_key(api_key)
         transaction = (
             TransactionBuilder()
             .builder()
@@ -71,3 +84,4 @@ def make_transaction(
             status_code=409,
             content={"error": {"message": err.args}},
         )
+
