75c45b2647173147e1756f7a116680da57d778f6
Giorgi Megrelidze <gmegr20@freeuni.edu.ge>
2024-02-04 14:22:29 +0400

    Switched wallet & transaction repositories to persistent

--- wallet/runner/setup.py
+++ wallet/runner/setup.py
@@ -4,10 +4,10 @@ from wallet.core.facade import TransactionService, UserService, WalletService
 from wallet.infra.fastapi.users_api import users_api
 from wallet.infra.fastapi.wallets_api import wallet_api
 from wallet.infra.fastapi.transactions_api import transactions_api 
-from wallet.infra.repository.memory.transaction_repository import TransactionRepository
-from wallet.infra.repository.memory.wallet_repository import WalletRepository
 from wallet.infra.repository.sqlite.connection_manager import ConnectionManager
 from wallet.infra.repository.sqlite.user_repository import UserRepository
+from wallet.infra.repository.sqlite.transaction_repository import TransactionRepository
+from wallet.infra.repository.sqlite.wallet_repository import WalletRepository
 
 
 def setup() -> FastAPI:
